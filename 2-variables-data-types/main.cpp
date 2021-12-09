c4afbe623793da0f616ca2e2893854ef9923fa62
Inbar Vernia <inbar.vernia@gmail.com>
2021-12-09 23:19:13 +0200

    Started section 2: basics of variable declaration and assignment/reassignment

---
+++ 2-variables-data-types/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    //To declare a variable, we must declare the variable type followed by the variable name and assigning a value
+    int x = 10;
+    // And it can easily be assigned a new value like so:
+    x = 90;
+    x = 20;
+    // But `x = "Hello";` wouldn't work because C++ is statically typed, meaning a variable can't have a different data type assigned as its value (in this case, x is an int so cannot be assigned to a string value)
+
+
+    return 0;
+}
