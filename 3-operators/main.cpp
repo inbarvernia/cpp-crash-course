18029a82f8b030af7df009ed4bd94604e55104ac
Inbar Vernia <inbar.vernia@gmail.com>
2021-12-22 17:28:47 +0200

    Section 3: Added examples for Comparison Operators

--- 3-operators/main.cpp
+++ 3-operators/main.cpp
@@ -7,36 +7,46 @@ int main() {
   // std::cout << a * b / 5 + 2 << std::endl;
   // std::cout << b % a << std::endl;
 
-  // Assignment Operators
-  int a = 20;
-  // The = above is an assignment operator
-  a = a + 10;
-  // As with other programming languages, you can use the assignment operator in combination with arithmetic operators, e.g. +=:
-  a += 12;
-  std::cout << a << std::endl;
-  // Just as you can use +=, *=, -=, /=, you can also use %=:
-  a %= 2;
-  std::cout << a << std::endl;
-  a += 20;
-  // You can combine the assignment operator not just with arithmetic operators but also with other operator groups, such as bitwise operators (which we won't discuss here, but as an example):
-  a &= 10;
-  std::cout << a << std::endl;
-  // Essentially, you can combine the assignment operator with other operators to save the results of the operation by assigning them to the variable
+  // // Assignment Operators
+  // int a = 20;
+  // // The = above is an assignment operator
+  // a = a + 10;
+  // // As with other programming languages, you can use the assignment operator in combination with arithmetic operators, e.g. +=:
+  // a += 12;
+  // std::cout << a << std::endl;
+  // // Just as you can use +=, *=, -=, /=, you can also use %=:
+  // a %= 2;
+  // std::cout << a << std::endl;
+  // a += 20;
+  // // You can combine the assignment operator not just with arithmetic operators but also with other operator groups, such as bitwise operators (which we won't discuss here, but as an example):
+  // a &= 10;
+  // std::cout << a << std::endl;
+  // // Essentially, you can combine the assignment operator with other operators to save the results of the operation by assigning them to the variable
   
-  // You can also use increment and decrement operators (++, --), as in JavaScript
-  int b = 10;
-  b++;
-  std::cout << b << std::endl;
-  // In C++, you can increment by using either b++ or ++b (or b += 1, or b = b + 1)
-  // The difference is in what is being returned: in b++, the value of b is returned and then incremented
-  std::cout << b++ << std::endl;
-  // The value printed will be the previous value of b; however, if we then print the current value of b, we will see it's higher by 1:
-  std::cout << b << std::endl;
-  // However, if we use ++b, the value will be incremented first and returned already with its updated value:
-  std::cout << ++b << std::endl;
-  // And since the value has already been updated, it will remain the same:
-  std::cout << b << std::endl;
-  // This same logic applies for decrementing (b--)
+  // // You can also use increment and decrement operators (++, --), as in JavaScript
+  // int b = 10;
+  // b++;
+  // std::cout << b << std::endl;
+  // // In C++, you can increment by using either b++ or ++b (or b += 1, or b = b + 1)
+  // // The difference is in what is being returned: in b++, the value of b is returned and then incremented
+  // std::cout << b++ << std::endl;
+  // // The value printed will be the previous value of b; however, if we then print the current value of b, we will see it's higher by 1:
+  // std::cout << b << std::endl;
+  // // However, if we use one of the other syntax options (++b or (b += 1)), the value will be incremented first and returned already with its updated value:
+  // std::cout << (b += 1) << std::endl;
+  // // And since the value has already been updated, it will remain the same:
+  // std::cout << b << std::endl;
+  // // This same logic applies for decrementing (b--)
+
+  // Comparison Operators (Relational): == != > < <= >= (These operators always return a boolean value)
+
+  int a = 10, b = 20;
+
+  std::cout << (a < b) << std:: endl;
+  // Evaluates as 1 which, as previously explained, represents true
+  std::cout << (a >= b) << std:: endl;
+  std::cout << (a != b) << std:: endl;
+
 
   return 0;
 }
