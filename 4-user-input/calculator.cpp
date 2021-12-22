58b1914774cd0a8395db639b1a25c2fb6eb9ed05
Inbar Vernia <inbar.vernia@gmail.com>
2021-12-23 00:29:33 +0200

    Section 4: Created simple sum calculator which takes user input

---
+++ 4-user-input/calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+  int n1, n2;
+  std::cout << "Please enter the first number: ";
+  std::cin >> n1;
+  std::cout << "Please enter the second number: ";
+  std::cin >> n2;
+
+  std::cout << "The sum is " << n1 + n2 << std::endl;
+
+  return 0;
+}
