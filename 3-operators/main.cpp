a85d8773d7683b2903c988c70a489eb60e436961
Inbar Vernia <inbar.vernia@gmail.com>
2021-12-22 16:53:43 +0200

    Section 3: Wrote examples for Arithmetic Operators

---
+++ 3-operators/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+  // Arithmetic Operators (+ - * / %)
+  int a = 10, b = 20;
+  std::cout << a * b / 5 + 2 << std::endl;
+  std::cout << b % a << std::endl;
+
+  return 0;
+}
