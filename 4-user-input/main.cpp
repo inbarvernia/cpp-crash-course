9e118d278c91b387f126c69d699b091898c300af
Inbar Vernia <inbar.vernia@gmail.com>
2021-12-23 00:22:44 +0200

    Section 4: Added examples for basic use of std::cin for user input

--- 4-user-input/main.cpp
+++ 4-user-input/main.cpp
@@ -2,6 +2,20 @@
 
 int main() {
 
+  std::cout << "Please enter your name: ";
+
+  std::string name;
+
+  std::cin >> name;
+
+  std::cout << "Please enter your age: ";
+
+  int age;
+
+  std::cin >> age;
+
+  std::cout << "Your name is " << name << std::endl;
+  std::cout << "You are " << age << " years old" << std::endl;
 
   return 0;
 }
