38b2d0e06ebc65be68f3551e6524b285c1643c70
Inbar Vernia <inbar.vernia@gmail.com>
2021-12-24 00:43:32 +0200

    Section 6: Added comments on remaining storage classes in other.cpp

---
+++ 6-storage-classes/other.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+  // For the sake of completion, the remaining storage classes which will not be discussed in detail here:
+  // Mutable storage class (to do with OOP)
+
+  // thread_local storage class (introduced around C++11)
+
+  return 0;
+}
