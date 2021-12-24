3d5aca0d03e90fe09a2f6ccc4973e582e8311729
Inbar Vernia <inbar.vernia@gmail.com>
2021-12-24 23:36:30 +0200

    Section 10: Added information on function overloading

--- 10-functions/main.cpp
+++ 10-functions/main.cpp
@@ -6,12 +6,16 @@ int add(int, int);
 // (It's also not considered best practice anyway, as the standard is to have the main function at the top, if there are even any other functions defined in the same file)
 // Therefore what we do is add function signatures for all supporting functions at the top of the file, and that way avoid the errors, because the function has already been declared and is therefore recognised when it's called inside another
 // The function signatures basically "tells" the programme that there is a function of that name that will be defined later on
+int add(int, int, int);
+std::string add(std::string, std::string);
 bool isGreaterThan(int, int);
 void myFunction();
 
 int main() {
 
   std::cout << add(10, 20) << std::endl;
+  std::cout << add(10, 20, 30) << std::endl;
+  std::cout << add("Hello ", "World!") << std::endl;
   std::cout << isGreaterThan(10, 5) << std::endl;
   myFunction();
   // If we tried cout << myFunction() we would get an exception, because myFunction doesn't return anything, which means that it cannot pass any data into cout
@@ -22,6 +26,20 @@ int main() {
 
 int add(int x, int y) {
   return x + y;
+}
+
+int add(int x, int y, int z) {
+  // Declaring multiple functions with the same name is possible in C++ and is known as function overloading
+
+  std::cout << "Using three parameters!" << std::endl;
+  // The line above was added mostly to demonstrate that it can be a completely different function from the other add function, it doesn't have to be related
+  return x + y + z;
+}
+
+std::string add(std::string s1, std::string s2) {
+  // With function overloading, not only can we have multiple functions with the same name, but they don't even have to use the same data types (either as return values or as parameters)
+  // However, if we tried to define another add function that takes the same number and type of arguments as another one, e.g. std::string add(int x, int y), we would get an error due to ambiguation - when we call add(10, 20), the programme has no way of knowing which of the add functions taking two ints as params we want - the one returning an int or the one returning a string
+  return s1 + s2;
 }
 
 bool isGreaterThan(int x, int y) {
