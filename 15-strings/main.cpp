a7e684a7248006f5e800074dcb6e66df16f10714
Inbar Vernia <inbar.vernia@gmail.com>
2021-12-28 18:39:41 +0200

    Section 15: Added example and risk assessment of strcpy

--- 15-strings/main.cpp
+++ 15-strings/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string.h>
+// The above module gives access to additional functions/methods available for strings
 
 int main() {
 
@@ -18,6 +20,19 @@ int main() {
   // The above line is implicit and will essentially be added automatically if not added manually - but if we declared char moreText[3] it would cause issues because we are not leaving an available char for '\0'
   
   std::cout << moreText << std::endl;
+  
+  std::cout << sizeof(moreText) << std::endl;
+  // The above gives the size of moreText in bytes (and on systems where char = 1 byte, it should be 4, but could be something different on systems where chars have a different size in bytes)
+  std::cout << strlen(moreText) << std::endl;
+  // The above uses the strlen (string length) function, which we can access through the string.h library/module; this function ignores the null terminator '\0', so we should get 3
+
+  // Even though we can still use C strings in C++, it's less recommended partly because of security issues; for examples:
+  char text1[16];
+  char text2[200];
+  std::cin >> text2;
+  // strcpy is a function from the string.h module which copies the content of a C string into another C string
+  strcpy(text1, text2);
+  // Because we're taking a string of 200 chars and copying it into a string with max 16 chars, we won't get an error, but will instead go over the bounds, which can cause issues, as previously explained
 
   return 0;
 }
