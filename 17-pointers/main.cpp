1b74e0adad39b69c16b2a60bad770a65cce67640
Inbar Vernia <inbar.vernia@gmail.com>
2022-01-07 20:56:30 +0000

    Section 17: Examples for addresses and pointers for arrays

--- 17-pointers/main.cpp
+++ 17-pointers/main.cpp
@@ -21,5 +21,21 @@ int main() {
 
   std::cout << a << std::endl;
 
+  int arr[100];
+  // When we creat an array, we are allocating the space for the number of elements and the data type, in this example, 400 bytes, or 100 times 4 bytes (the size of an int)
+  // In doing this, an address is allocated for the first item, and then adjacent addresses are allocated to subsequent values
+  
+  // The address for the array itself is the same as the address for the first item:
+  std::cout << &arr << std::endl;
+  std::cout << &arr[0] << std::endl;
+  // And we can see that the addresses for the next items follows in increments of 4, allowing for 4 bytes of data for each:
+  std::cout << &arr[1] << std::endl;
+  std::cout << &arr[2] << std::endl;
+
+  // When creating a pointer for an array, we don't need to use the & symbol:
+  int *arrptr = arr;
+
+  std::cout << arrptr << std::endl;
+
   return 0;
 }
