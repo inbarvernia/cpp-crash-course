24382c801288d40e9161a8f63ba2fb63b5751e8d
Inbar Vernia <inbar.vernia@gmail.com>
2022-01-27 16:50:20 +0000

    Section 19: Added example of defining macros

--- 19-pre-processor-directives/main.cpp
+++ 19-pre-processor-directives/main.cpp
@@ -11,12 +11,19 @@
 // The difference between this and defining a constant or variable is that here every occurrence of the identifier PI is replaced with the number in parenthesis *before* the programme is compiled
 // #define can also contain strings or other data types:
 #define SHARED_MEMORY_NAME "myshared_mem"
-
+// #define creates what is known as a macro
+// Macros are useful for defining some data that will not be changed and which needs to be available globally (a little bit like a globally available constant, although there are other differences between constants and macros; see for discussion: https://stackoverflow.com/questions/6393776/what-is-the-difference-between-a-macro-and-a-const-in-c)
+// One example of an appropriate use of macros:
+# define MAX_ARR_SIZE (16) // used in conjunction with int array below
 
 int main() {
 
   std::cout << PI << '\n';
   std::cout << SHARED_MEMORY_NAME << '\n';
+  
+  int arr[MAX_ARR_SIZE];
+
+
 
   return 0;
 }
